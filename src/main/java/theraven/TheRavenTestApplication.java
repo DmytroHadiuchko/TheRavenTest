9dd5872c93dd1a8586873ab203dc1027a7835a82
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-07-01 16:22:55 +0300

    the project was created

---
+++ src/main/java/theraven/TheRavenTestApplication.java
@@ -0,0 +1,13 @@
+package theraven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRavenTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheRavenTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/theraven/TheRavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package theraven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRavenTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
