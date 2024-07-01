16130b51337720b1d42adc05b0c1f2e6e74f5ec2
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-07-01 17:28:05 +0300

    created customer entity, add dependency

---
+++ src/main/java/theraven/entity/Customer.java
@@ -0,0 +1,33 @@
+package theraven.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "customers")
+@SQLDelete(sql= "UPDATE customers SET is_active = false WHERE id=?")
+@SQLRestriction(value = "is_active=true")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "full_name")
+    private String fullName;
+    private String email;
+    private String phone;
+    @Column(name = "is_active", nullable = false, columnDefinition = "TINYINT(1)")
+    private boolean is_active;
+    private Long created;
+    private Long updated;
+
+}
